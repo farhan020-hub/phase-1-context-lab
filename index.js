e0414e2e73b96fae15ca013d171970a3008e31c6
AbdiazizSaman <farhanadam394@gmail.com>
2023-10-07 21:05:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,79 @@
 /* Your Code Here */
 
+
+function createEmployeeRecord(array) {
+    const employeeRecord = {
+      firstName: array[0],
+      familyName: array[1],
+      title: array[2],
+      payPerHour: array[3],
+      timeInEvents: [],
+      timeOutEvents: [],
+    };
+    return employeeRecord;
+  }
+
+  function createEmployeeRecords(arrays) {
+    const employeeRecords = arrays.map((array) => {
+      return createEmployeeRecord(array);
+    });
+    return employeeRecords.slice(0, 10); 
+  }
+
+function createTimeInEvent(dateTimeString) {
+    let [date, hour] = dateTimeString.split(' ')
+
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return this
+}
+
+function createTimeOutEvent(dateTimeString) {
+    let [date, hour] = dateTimeString.split(' ')
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return this
+}
+
+function hoursWorkedOnDate(date) {
+    const timeInEvent = this.timeInEvents.find(
+      (event) => event.date === date
+    );
+    const timeOutEvent = this.timeOutEvents.find(
+      (event) => event.date === date
+    );
+    const hoursWorked = (timeOutEvent.hour - timeInEvent.hour) / 100;
+    return hoursWorked;
+  }
+
+ 
+  function wagesEarnedOnDate (date) {
+    let wage = hoursWorkedOnDate.call(this, date)
+        * this.payPerHour
+    return parseFloat(wage.toString())
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(function (rec) {
+      return rec.firstName === firstName;
+    });
+  }
+
+function calculatePayroll (arrayOfEmployeeRecords){
+    return arrayOfEmployeeRecords.reduce(function(memo, rec){
+        return memo + allWagesFor.call(rec)
+    }, 0)
+}
+  
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
